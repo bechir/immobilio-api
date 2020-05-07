9f403f92f9dd6579d0e9b4db600ba87078c09e04
rand0mdev <bechiirr71@gmail.com>
2020-05-07 07:49:35 +0000

    Refactor

--- src/Controller/Api/EtatController.php
+++ src/Controller/Api/EtatController.php
@@ -8,7 +8,6 @@
 namespace App\Controller\Api;
 
 use App\Repository\CmlFactureRepository;
-use App\Repository\CptOperationCaisseRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -55,131 +54,57 @@ class EtatController extends ApiController
         ];
 
         return $this->json(
-            $cmlFactureRepository->getArrieresByClientIdByAgenceCodeByDate($params)
+            $cmlFactureRepository->getEtatArrieresByClientIdByAgenceCodeByDate($params)
+        );
+    }
+
+    /**
+     * ENCAISSEMENT DANS LES AGENCES SUR UNE PERIODE.
+     *
+     * @param string|null clientId:     L'id du client
+     * @param string|null agenceCode:   Le code de l'agence
+     * @param string|null startDate:    Date de début
+     * @param string|null endDate:      Date de fin
+     *
+     * @Route("/encaissements")
+     */
+    public function getEncaissementsByClientIdByAgenceCodeByDate(Request $request, CmlFactureRepository $cmlFactureRepository)
+    {
+        $query = $request->query;
+        $params = [
+            'clientId'  =>  $query->get('clientId'),
+            'agenceCode'  =>  $query->get('agenceCode'),
+            'startDate' =>  $query->get('startDate'),
+            'endDate'   =>  $query->get('endDate')
+        ];
+
+        return $this->json(
+            $cmlFactureRepository->getEtatEncaissementsByClientIdByAgenceCodeByDate($params)
         );
     }
 
     /**
      * DECAISSEMENT DANS LES AGENCES SUR UNE PERIODE.
      *
-     * @param string dateDebut: Date de debut
-     * @param string dateFin: Date de fin
+     * @param string|null clientId:     L'id du client
+     * @param string|null agenceCode:   Le code de l'agence
+     * @param string|null startDate:    Date de début
+     * @param string|null endDate:      Date de fin
      *
-     * @Route("/decaissement/par-nature/{dateDebut}/{dateFin}")
+     * @Route("/decaissements")
      */
-    public function getDecaissementParNatureePeriode(CptOperationCaisseRepository $cptOperationCaisseRepository, string $dateDebut, string $dateFin)
+    public function getDecaissementsByClientIdByAgenceCodeByDate(Request $request, CmlFactureRepository $cmlFactureRepository)
     {
-        $result = $cptOperationCaisseRepository->getEncDecParNaturePeriode(8, $dateDebut, $dateFin);
+        $query = $request->query;
+        $params = [
+            'clientId'  =>  $query->get('clientId'),
+            'agenceCode'  =>  $query->get('agenceCode'),
+            'startDate' =>  $query->get('startDate'),
+            'endDate'   =>  $query->get('endDate')
+        ];
 
-        return $this->json($result);
-    }
-
-    /**
-     * DECAISSEMENT PAR NATURE DANS UNE AGENCE (exemple DOUALA) SUR UNE PERIODE.
-     *
-     * @param int agenceId: Id de l'agence
-     * @param string dateDebut: Date de debut
-     * @param string dateFin: Date de fin
-     *
-     * @Route("/decaissement/par-nature/agence/{agenceId}/{dateDebut}/{dateFin}")
-     */
-    public function getDecaissementParNatureAgencePeriode(CptOperationCaisseRepository $cptOperationCaisseRepository, int $agenceId, string $dateDebut, string $dateFin)
-    {
-        $result = $cptOperationCaisseRepository->getEncDecParNatureAgencePeriode(8, $agenceId, $dateDebut, $dateFin);
-
-        return $this->json($result);
-    }
-
-    /**
-     * DECAISSEMENT SUR LES IMMEUBLES D'UNE AGENCE (exemple agence_id = 1) SUR UNE PERIODE.
-     *
-     * @param int agenceId: Id de l'agence
-     * @param string dateDebut: Date de debut
-     * @param string dateFin: Date de fin
-     *
-     * @Route("/decaissement/par-immeuble/agence/{agenceId}/{dateDebut}/{dateFin}")
-     */
-    public function getDecaissementParImmeubleAgencePeriode(CptOperationCaisseRepository $cptOperationCaisseRepository, int $agenceId, string $dateDebut, string $dateFin)
-    {
-        $result = $cptOperationCaisseRepository->getEncDecParImmeubleAgencePeriode(8, $agenceId, $dateDebut, $dateFin);
-
-        return $this->json($result);
-    }
-
-    /**
-     * ENCAISSEMENT DANS LES AGENCES SUR UNE PERIODE.
-     *
-     * @param string dateDebut: Date de debut
-     * @param string dateFin: Date de fin
-     *
-     * @Route("/encaissement/par-nature/{dateDebut}/{dateFin}")
-     */
-    public function getEncaissementParNatureePeriode(CptOperationCaisseRepository $cptOperationCaisseRepository, string $dateDebut, string $dateFin)
-    {
-        $result = $cptOperationCaisseRepository->getEncDecParNaturePeriode(7, $dateDebut, $dateFin);
-
-        return $this->json($result);
-    }
-
-    /**
-     * ENCAISSEMENT PAR NATURE DANS UNE AGENCE (exemple DOUALA) SUR UNE PERIODE.
-     *
-     * @param int agenceId: Id de l'agence
-     * @param string dateDebut: Date de debut
-     * @param string dateFin: Date de fin
-     *
-     * @Route("/encaissement/par-nature/agence/{agenceId}/{dateDebut}/{dateFin}")
-     */
-    public function getEncaissementParNatureAgencePeriode(CptOperationCaisseRepository $cptOperationCaisseRepository, int $agenceId, string $dateDebut, string $dateFin)
-    {
-        $result = $cptOperationCaisseRepository->getEncDecParNatureAgencePeriode(7, $agenceId, $dateDebut, $dateFin);
-
-        return $this->json($result);
-    }
-
-    /**
-     * ENCAISSEMENT SUR LES IMMEUBLES D'UNE AGENCE (exemple agence_id = 1) SUR UNE PERIODE.
-     *
-     * @param int agenceId: Id de l'agence
-     * @param string dateDebut: Date de debut
-     * @param string dateFin: Date de fin
-     *
-     * @Route("/encaissement/par-immeuble/agence/{agenceId}/{dateDebut}/{dateFin}")
-     */
-    public function getEncaissementParImmeubleAgencePeriode(CptOperationCaisseRepository $cptOperationCaisseRepository, int $agenceId, string $dateDebut, string $dateFin)
-    {
-        $result = $cptOperationCaisseRepository->getEncDecParImmeubleAgencePeriode(7, $agenceId, $dateDebut, $dateFin);
-
-        return $this->json($result);
-    }
-
-    /**
-     * ETAT DES DECAISSEMENTS PAR AGENCE (exemple agence_id = 1) SUR UNE ANNEE (exemple 2020) ET REPARTIS PAR MOIS.
-     *
-     * @param int agenceId: Id de l'agence
-     * @param int annee: L'année cherchée
-     *
-     * @Route("/decaissement/agence/{agenceId}/{annee}")
-     */
-    public function getEtatDecaissementAgenceParMois(CptOperationCaisseRepository $cptOperationCaisseRepository, int $agenceId, int $annee)
-    {
-        $result = $cptOperationCaisseRepository->getEncEtatDecAgenceParMois(8, $agenceId, $annee);
-
-        return $this->json($result);
-    }
-
-    /**
-     * ETAT DES ENCAISSEMENTS PAR AGENCE (exemple agence_id = 1) SUR UNE ANNEE (exemple 2020) ET REPARTIS PAR MOIS.
-     *
-     * @param int agenceId: Id de l'agence
-     * @param int annee: L'année cherchée
-     *
-     * @Route("/encaissement/agence/{agenceId}/{annee}")
-     */
-    public function getEtatEncaissementAgenceParMois(CptOperationCaisseRepository $cptOperationCaisseRepository, int $agenceId, int $annee)
-    {
-        $result = $cptOperationCaisseRepository->getEncEtatDecAgenceParMois(7, $agenceId, $annee);
-
-        return $this->json($result);
+        return $this->json(
+            $cmlFactureRepository->getEtatDecaissementsByClientIdByAgenceCodeByDate($params)
+        );
     }
 }
--- src/Repository/CmlFactureRepository.php
+++ src/Repository/CmlFactureRepository.php
@@ -13,9 +13,9 @@ use App\Entity\CmlFactureEspace;
 use App\Entity\CptOperationCaisse;
 use App\Entity\PatBienImmobilier;
 use App\Entity\PatEspace;
-use App\Entity\PatSci;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\Query\Expr\Join;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -38,86 +38,54 @@ class CmlFactureRepository extends ServiceEntityRepository
             ->getQuery()->getResult();
     }
 
-    public function getArriereClientParFacture(string $type, string $id, ?string $date)
+    public function getEtatEncaissementsByClientIdByAgenceCodeByDate(?array $params)
     {
-        if (!$date) {
-            $date = date('Y-m-d');
-        }
+        $qb = $this->getBaseFactureQueryBuilder()
+        ->select('SUBSTRING(f.dateFacture, 1, 10) as dateDernierPaiement')
+        ->addSelect('c.nom as nomClient')
+        ->addSelect('c.personnePrincipalTel1 as contactClient')
+        ->addSelect('f.montantTotalNet as montant')
+        ->addSelect('opCaisse.numFacturePiece')
+        ->addSelect('agence.nom as nomAgence')
+        ->addSelect('patSci.libelle as sci')
+        ->addSelect('count(f.id) as totalEncaissements')
+        ->addSelect('case when SUM(opCaisse.montant) IS NULL then 0 else SUM(opCaisse.montant) END as montantDernierPaiement')
+        ->addSelect('factEspace.loyerMensuel as loyer')
+        ->addSelect('factEspace.caution as caution')
+        ->addSelect('factEspace.nombreMois as nombreMois')
+        ->addSelect('pat.libelle as espaceLoue')
 
-        $qb = $this->getBaseFactureQueryBuilder($date);
-
-        if ('client' === $type) {
-            $qb->andWhere('c.id = :id')
-            ->setParameter('id', $id);
-        } elseif ('agence' === $type) {
-            $qb->andWhere('f.codeAgence = :code')
-            ->setParameter('code', $id);
-        }
-
-        return $qb->getQuery()->getResult();
+        ->where("statusOperationCaisse.id = 1")
+        ->andWhere('typeOpCaisse.id = 8');
+        
+        return $this->bindEtatFilters($qb, $params);
     }
 
-    public function getBaseFactureQueryBuilder(?string $date)
+    public function getEtatArrieresByClientIdByAgenceCodeByDate(?array $params)
     {
-        if (!$date) {
-            $date = date('Y-m-d');
-        }
+        $qb = $this->getBaseFactureQueryBuilder()
+        ->select('SUBSTRING(f.dateFacture, 1, 10) as dateDernierPaiement')
+        ->addSelect('c.nom as nomClient')
+        ->addSelect('c.personnePrincipalTel1 as contactClient')
+        ->addSelect('f.montantTotalNet as montant')
+        ->addSelect('opCaisse.numFacturePiece')
+        ->addSelect('agence.nom as nomAgence')
+        ->addSelect('patSci.libelle as sci')
+        ->addSelect('f.montantTotalNet - case when SUM(opCaisse.montant) IS NULL then 0 else SUM(opCaisse.montant) END as montantDu')
+        ->addSelect('case when SUM(opCaisse.montant) IS NULL then 0 else SUM(opCaisse.montant) END as montantDernierPaiement')
+        ->addSelect('factEspace.loyerMensuel as loyer')
+        ->addSelect('factEspace.caution as caution')
+        ->addSelect('factEspace.nombreMois as nombreMois')
+        ->addSelect('pat.libelle as espaceLoue')
 
-        return $this->createQueryBuilder('f')
-            ->leftJoin('f.client', 'c')->addSelect('c')
-            ->leftJoin('f.status', 's')->addSelect('s')
+        ->where("s.code IN ('SF001','SF002')");
 
-            ->leftJoin(AppAgence::class, 'agence', Join::WITH, 'agence.code = f.codeAgence')
-            ->leftJoin(CmlFactureEspace::class, 'factEspace', Join::WITH, 'factEspace.facture = f')
-            ->leftJoin(CptOperationCaisse::class, 'opCaisse', Join::WITH, 'f.reference = opCaisse.numFacturePiece')
-            ->leftJoin('opCaisse.statusOperation', 'statusOperationCaisse')->addSelect('statusOperationCaisse')
-
-            ->select('c.id as id_client')
-            ->addSelect('c.emailClient email_client')
-            ->addSelect('c.telClient tel_client')
-            ->addSelect('c.raisonSociale raison_sociale')
-            ->addSelect('f.reference as reference')
-            ->addSelect('f.montantTotalNet as montant_total_net')
-            ->addSelect('SUBSTRING(f.dateFacture, 1, 10) as date_facture')
-            ->addSelect('SUBSTRING(f.dateLimite, 1, 10) as date_limite')
-            ->addSelect('s.libelle as libelle_status')
-            ->addSelect('agence.nom as nom_agence')
-            ->addSelect('SUM(opCaisse.montant) as montant_deja_paye')
-            ->addSelect('f.montantTotalNet - SUM(opCaisse.montant) as montant_restant')
-            ->addSelect('factEspace.nombreMois as nombre_mois_facture')
-
-            ->where("s.code IN ('SF001','SF002')")
-            ->andWhere('f.deleted = 0')
-            ->andWhere('f.dateFacture < :dateFacture')
-            ->setParameter('dateFacture', $date)
-
-            ->groupBy('reference')
-            ->addGroupBy('montant_total_net')
-            ->addGroupBy('date_facture')
-            ->addGroupBy('date_limite')
-            ->addGroupBy('nom_agence')
-            ->addGroupBy('libelle_status')
-            ->addGroupBy('nombre_mois_facture')
-            ->addGroupBy('id_client')
-            ->addGroupBy('raison_sociale')
-            ->addGroupBy('tel_client')
-            ->addGroupBy('email_client');
+        return $this->bindEtatFilters($qb, $params);
     }
 
-    public function getArrieresByClientIdByAgenceCodeByDate(?array $params)
+    public function getEtatDecaissementsByClientIdByAgenceCodeByDate(?array $params)
     {
-        $qb = $this->createQueryBuilder('f')
-        ->leftJoin('f.client', 'c')->addSelect('c')
-        ->leftJoin('f.status', 's')->addSelect('s')
-        ->leftJoin(AppAgence::class, 'agence', Join::WITH, 'agence.code = f.codeAgence')
-        ->leftJoin(CmlFactureEspace::class, 'factEspace', Join::WITH, 'factEspace.facture = f')
-        ->leftJoin(CptOperationCaisse::class, 'opCaisse', Join::WITH, 'f.reference = opCaisse.numFacturePiece')
-        ->leftJoin(CmlFactureEspace::class, 'espace', Join::WITH, 'espace.facture = f.id')
-        ->leftJoin(PatEspace::class, 'pat', Join::WITH, 'pat.id = espace.espace')
-        ->leftJoin(PatBienImmobilier::class, 'bien', Join::WITH, 'bien.id = pat.bienImmobilier')
-        ->leftJoin('bien.sci', 'patSci')->addSelect('patSci')
-        ->leftJoin('opCaisse.statusOperation', 'statusOperationCaisse')->addSelect('statusOperationCaisse')
-
+        $qb = $this->getBaseFactureQueryBuilder()
         ->select('SUBSTRING(f.dateFacture, 1, 10) as date')
         ->addSelect('opCaisse.beneficiaire as beneficiaire')
         ->addSelect('opCaisse.beneficiaire as contact')
@@ -126,13 +94,15 @@ class CmlFactureRepository extends ServiceEntityRepository
         ->addSelect('agence.nom as nomAgence')
         ->addSelect('patSci.libelle as sci')
 
-        // ->addSelect('SUM(opCaisse.montant) as montant_deja_paye')
-        // ->addSelect('f.montantTotalNet - SUM(opCaisse.montant) as montant_restant')
-        // ->addSelect('factEspace.nombreMois as nombre_mois_facture')
-
-        ->where("s.code IN ('SF001','SF002')")
+        ->where("statusOperationCaisse.id = 1")
+        ->andWhere('typeOpCaisse.id = 8')
         ->andWhere('f.deleted = 0');
 
+        return $this->bindEtatFilters($qb, $params);
+    }
+
+    public function bindEtatFilters(QueryBuilder $qb, array $params)
+    {
         if($params['clientId']) {
             $qb->andWhere('c.id = :client')->setParameter('client', $params['clientId']);
         }
@@ -150,6 +120,25 @@ class CmlFactureRepository extends ServiceEntityRepository
         }
 
         return $qb->groupBy('c.id')->getQuery()->getResult();
+    }
+
+    public function getBaseFactureQueryBuilder()
+    {
+        return $this->createQueryBuilder('f')
+            ->leftJoin('f.client', 'c')->addSelect('c')
+            ->leftJoin('f.status', 's')->addSelect('s')
+
+            ->leftJoin(AppAgence::class, 'agence', Join::WITH, 'agence.code = f.codeAgence')
+            ->leftJoin(CmlFactureEspace::class, 'factEspace', Join::WITH, 'factEspace.facture = f')
+            ->leftJoin(CptOperationCaisse::class, 'opCaisse', Join::WITH, 'f.reference = opCaisse.numFacturePiece')
+            ->leftJoin(CmlFactureEspace::class, 'espace', Join::WITH, 'espace.facture = f.id')
+            ->leftJoin(PatEspace::class, 'pat', Join::WITH, 'pat.id = espace.espace')
+            ->leftJoin(PatBienImmobilier::class, 'bien', Join::WITH, 'bien.id = pat.bienImmobilier')
+            ->leftJoin('bien.sci', 'patSci')->addSelect('patSci')
+            ->leftJoin('opCaisse.statusOperation', 'statusOperationCaisse')->addSelect('statusOperationCaisse')
+            ->leftJoin('opCaisse.typeOperationCaisse', 'typeOpCaisse')->addSelect('typeOpCaisse')
+
+            ->andWhere('f.deleted = 0');
     }
 
     public function getFacturesByClient($clientId, $startDate, $endDate)
